3ca8b2083dbf530db911455b78d8fa9ba9a3e86e
--replace-all <hishammoonnakal@gmail.com>
2021-06-24 12:10:48 +0530

    observer_change

--- lib/observer.dart
+++ lib/observer.dart
@@ -4,24 +4,25 @@ class GlobalObserver extends BlocObserver {
   @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
-    print('${bloc.runtimeType} $event');
+    if (bloc.state != null) {}
+    // print('${bloc.runtimeType} $event');
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    print('${bloc.runtimeType} $change');
+    if (bloc.state != null) {}
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    print('${bloc.runtimeType} $transition');
+    if (bloc.state != null) {}
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    print('${bloc.runtimeType} $error $stackTrace');
     super.onError(bloc, error, stackTrace);
+    if (bloc.state != null) {}
   }
 }
