f5472dedb8843b8b2cb08dcf25e1ab21ddc33641
--replace-all <hishammoonnakal@gmail.com>
2021-06-27 17:21:46 +0530

    register_widget-refactor

--- lib/register/view/widgets/register_form_inputs.dart
+++ lib/register/view/widgets/register_form_inputs.dart
@@ -16,44 +16,47 @@ List<String> inputs = [
 ];
 List<Widget> inputList = [];
 
-Widget textInput(dynamic data) => TextFormField(
-      key: Key(data.toString()),
-      textInputAction:
-          data != 'About Shop' ? TextInputAction.next : TextInputAction.done,
-      maxLines: data != 'About Shop' ? 1 : 3,
-      keyboardType: ['Pin Code'].contains(data)
-          ? TextInputType.number
-          : ['Phone Number'].contains(data)
-              ? TextInputType.phone
-              : ['Email ID'].contains(data)
-                  ? TextInputType.emailAddress
-                  : ['About Shop'].contains(data)
-                      ? TextInputType.multiline
-                      : TextInputType.name,
-      keyboardAppearance: Brightness.dark,
-      validator: (value) {
-        if (value == null || value.isEmpty) {
-          return 'Please enter $data';
-        }
-        return null;
-      },
-      obscureText: data == 'Password' || data == 'Password Confirmation',
-      decoration: InputDecoration(
-        enabledBorder: const OutlineInputBorder(
-            borderSide: BorderSide(color: Color(0xFFEAEEF7)),
-            borderRadius: BorderRadius.all(Radius.circular(10.0))),
-        fillColor: const Color(0xFFEAEEF7),
-        border: const OutlineInputBorder(),
-        filled: true,
-        hintText: data.toString(),
-        hintMaxLines: data != 'About Shop' ? 1 : 3,
-      ),
-    );
+Widget textInput(dynamic data) {
+  return TextFormField(
+    key: Key(data.toString()),
+    textInputAction:
+        data != 'About Shop' ? TextInputAction.next : TextInputAction.done,
+    maxLines: data != 'About Shop' ? 1 : 3,
+    keyboardType: ['Pin Code'].contains(data)
+        ? TextInputType.number
+        : ['Phone Number'].contains(data)
+            ? TextInputType.phone
+            : ['Email ID'].contains(data)
+                ? TextInputType.emailAddress
+                : ['About Shop'].contains(data)
+                    ? TextInputType.multiline
+                    : TextInputType.name,
+    keyboardAppearance: Brightness.dark,
+    validator: (value) {
+      if (value == null || value.isEmpty) {
+        return 'Please enter $data';
+      }
+      return null;
+    },
+    obscureText: data == 'Password' || data == 'Password Confirmation',
+    decoration: InputDecoration(
+      enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFFEAEEF7)),
+          borderRadius: BorderRadius.all(Radius.circular(10.0))),
+      fillColor: const Color(0xFFEAEEF7),
+      border: const OutlineInputBorder(),
+      filled: true,
+      hintText: data.toString(),
+      hintMaxLines: data != 'About Shop' ? 1 : 3,
+    ),
+  );
+}
+
 Widget paddingz = const SizedBox(
   height: 12,
 );
 
-void getInputs() {
+Future<void> getInputs() async {
   inputList.clear();
   inputs
       .map<dynamic>((e) => inputList.addAll([textInput(e), paddingz]))
