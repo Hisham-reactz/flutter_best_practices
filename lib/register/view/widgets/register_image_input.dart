568e561af7ffc26558862cf108ea3c7c02309b04
--replace-all <hishammoonnakal@gmail.com>
2021-06-30 11:47:00 +0530

    reg_img_input_clear

--- lib/register/view/widgets/register_image_input.dart
+++ lib/register/view/widgets/register_image_input.dart
@@ -33,10 +33,22 @@ Widget imageInput(BuildContext context) {
                         )))
                 : Expanded(
                     flex: 1,
-                    child: Image.file(
-                      File(state.image1),
-                      fit: BoxFit.contain,
-                    )),
+                    child: Stack(children: [
+                      Image.file(
+                        File(state.image1),
+                        fit: BoxFit.contain,
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          context.read<RegisterBloc>().add(ImageChanged(
+                              '', state.image2.isNotEmpty ? state.image2 : ''));
+                        },
+                        icon: const Icon(
+                          Icons.close,
+                          color: Colors.orangeAccent,
+                        ),
+                      )
+                    ])),
             const VerticalDivider(),
             state.image2.isEmpty
                 ? Expanded(
@@ -56,10 +68,22 @@ Widget imageInput(BuildContext context) {
                           Icons.add_a_photo_outlined,
                           size: 37,
                         )))
-                : Image.file(
-                    File(state.image2),
-                    fit: BoxFit.contain,
-                  ),
+                : Stack(children: [
+                    Image.file(
+                      File(state.image2),
+                      fit: BoxFit.contain,
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        context.read<RegisterBloc>().add(ImageChanged(
+                            state.image1.isNotEmpty ? state.image1 : '', ''));
+                      },
+                      icon: const Icon(
+                        Icons.close,
+                        color: Colors.orangeAccent,
+                      ),
+                    )
+                  ]),
           ]),
           height: context.heightPct(0.2),
           decoration: const BoxDecoration(
